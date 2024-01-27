155574acce746188a5aca99df95c220a2f84b851
akashbhadouria <akashbhadouria85@gmail.com>
2024-01-27 20:35:06 +0530

    Adding changes in the navbar scroll and adding contact us btn

--- eco-rating/src/components/Nav/Nav.jsx
+++ eco-rating/src/components/Nav/Nav.jsx
@@ -1,42 +1,49 @@
-import React, { useState, useEffect } from "react";
-
-import "./Nav.css";
-import logo from "../../assets/images/footer_logo2.png";
+import React, { useState, useEffect } from 'react'
+import './Nav.css'
+import logo from '../../assets/images/footer_logo2.png'
 
 const Nav = () => {
-  const [isNavbarVisible, setNavbarVisible] = useState(true);
+  const [isNavbarVisible, setNavbarVisible] = useState(true)
+  const scrollPosition = window.scrollY
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const threshold = window.innerHeight * 0.3;
-
-      // Hide the navbar when scrolling beyond 30% of the viewport height
-      setNavbarVisible(scrollPosition < threshold);
-    };
+      const threshold = (scrollPosition / window.innerHeight) * 100
+      if (isNavbarVisible) {
+        setNavbarVisible(threshold >= 57)
+      } else {
+        setNavbarVisible(true)
+      }
+    }
 
     // Attach event listener for scroll events
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
     // Clean up the event listener when the component unmounts
     return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [scrollPosition])
 
   return (
-    <nav className={`navbar ${isNavbarVisible ? "visible" : "hidden"}`}>
-      <div className="logo-container">
-        <img src={logo} alt="Logo" className="logo" />
-      </div>
-      <ul>
-        <li>Gen AI</li>
-        <li>Clients</li>
-        <li>Method</li>
-        <li>About Us</li>
-      </ul>
-    </nav>
-  );
-};
+    <>
+      <nav className={`navbar ${isNavbarVisible ? 'visible' : 'hidden'}`}>
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <div className="li-wrapper">
+          <ul>
+            <li>Gen AI</li>
+            <li>Clients</li>
+            <li>Method</li>
+            <li>About Us</li>
+          </ul>
 
-export default Nav;
+          <button className="contact-us">Contact us</button>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Nav
