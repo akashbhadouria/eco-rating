d53daa115e46c296fe12af8b6908c681658d3c34
Sumit Dahiya <sumit@votan.eco>
2024-01-27 18:18:44 +0530

    added code for footer, nav, cover

---
+++ eco-rating/src/components/Cover/Cover.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Cover.css';
+import CoverImg from "../../assets/images/cover_pic.png";
+
+const Cover = () => {
+  return (
+    <div className="cover">
+      <img src={CoverImg} alt="Cover Image" className="cover-image" />
+      {/* <div className="overlay"></div> */}
+      <div className="cover-text-container">
+        <h1 className="cover-text">Helping Buyers Choose Sustainable Products and Services</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Cover;
---
+++ eco-rating/src/components/Nav/Nav.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+import './Nav.css';
+import logo from "../../assets/images/footer_logo2.png";
+
+const Nav = () => {
+  const [isNavbarVisible, setNavbarVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const threshold = window.innerHeight * 0.3;
+
+      // Hide the navbar when scrolling beyond 30% of the viewport height
+      setNavbarVisible(scrollPosition < threshold);
+    };
+
+    // Attach event listener for scroll events
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`navbar ${isNavbarVisible ? 'visible' : 'hidden'}`}>
+        <div className="logo-container">
+            <img src={logo} alt="Logo" className="logo" />
+        </div>
+      <ul>
+        <li>Gen AI</li>
+        <li>Clients</li>
+        <li>Method</li>
+        <li>About Us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
